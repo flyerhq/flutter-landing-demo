3cc78e8cd0afdacddebb3ce1f2ce613186f3926e
Alex Demchenko <alexdemchenko@yahoo.com>
2025-07-16 00:17:29 +0200

    add scroll

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "359479260712b2e014bd0dc5db93a820",
+const RESOURCES = {"flutter_bootstrap.js": "6d0c37b978017df79f9a0dfd58c56786",
 "version.json": "ff966ab969ba381b900e61629bfb9789",
 "index.html": "b11bbc4b4d1ae02a47a9103da8aeb805",
 "/": "b11bbc4b4d1ae02a47a9103da8aeb805",
-"main.dart.js": "37b26f5cbb8b0ab8a1ba0b950ea8ff96",
+"main.dart.js": "5d0e7834dff464e2fe0d2f678193bb09",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
